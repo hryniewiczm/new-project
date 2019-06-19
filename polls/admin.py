e4fcff1a27f87814ec8681b68932052f4096088e
Magdah <magdalena.hryniewicz84@gmail.com>
2019-06-19 13:18:14 +0200

    adding static files to polls app

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Choice, Question
 
-admin.site.register(Choice)
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+
 admin.site.register(Question)
+
+
+admin.site.register(Choice)
